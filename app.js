936e63774c45548f14b6dd13ea048c0d5be0fa82
Vinicius Silva <viniciuspdasilva.dev@gmail.com>
2025-12-17 20:19:38 -0300

    Refactor payment flow: - Removed unused `users` route. - Added `generateAuthCode` function for secure transaction IDs. - Enhanced `/payments` endpoint to use generated auth codes.

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var indexRouter = require('./routes/index');
 var logger = require('morgan');
+var crypto = require('crypto');
 
 var app = express();
+
+const secret = 'super-secret-key';
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -12,32 +16,44 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.post('/payments', (req, res) => {
-    const {orderId, amount, userId} = req.body;
+function generateAuthCode({orderId, amount, currency}) {
+    const payload = `${orderId}|${amount}|${currency}`;
+    // cria HMAC como Buffer
+    const mac = crypto.createHmac("sha256", secret).update(payload).digest(); // gera nonce como Buffer
+    const nonce = crypto.randomBytes(4);
+    const combined = Buffer.concat([nonce, mac.subarray(0, 4)]);
+    const code = BigInt("0x" + combined.toString("hex")).toString(36).toUpperCase();
+    return code.slice(0, 10); // ex.: "8G2X9K3PQT"
+}
 
-    const success = Math.random() > 0.2;
+    app.use('/', indexRouter);
+    app.post('/payments', (req, res) => {
+        const {orderId, amount, userId} = req.body;
 
-    if (success) {
-        res.status(200)
-            .json({
+        const success = Math.random() > 0.2;
+        const authCode = generateAuthCode({orderId, amount, userId});
+        if (success) {
+            res.status(200)
+                .json({
+                    orderId,
+                    amount,
+                    userId,
+                    transactionId: authCode,
+                    status: 'PAID'
+                });
+        } else {
+            res.status(400).json({
                 orderId,
                 amount,
                 userId,
-                transactionId: Math.floor(Math.random() * 1000000),
-                status: 'PAID'
+                status: 'FAILED',
+                message: 'not authorized'
             });
-    } else {
-        res.status(400).json({
-            orderId,
-            amount,
-            userId,
-            status: 'FAILED',
-            message: 'not authorized'
-        });
-    }
-});
+        }
+    });
 
-app.listen(3000, () => { console.log("Fake Payment API rodando na porta 3000"); });
+    app.listen(3000, () => {
+        console.log("Fake Payment API rodando na porta 3000");
+    });
 
-module.exports = app;
+    module.exports = app;

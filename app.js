6cdb0ab3144aaff2a486aa8b307046ae1e22dfca
Vinicius Silva <viniciuspdasilva.dev@gmail.com>
2025-12-17 19:27:47 -0300

    Initial setup of "fake-payment-api" application: - Added base Express.js app with routes for index and users. - Implemented mock `/pay` endpoint for payment simulation. - Included static files (HTML, CSS) and necessary dependencies. - Configured `.idea` project folder and gitignore.

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.post('/payments', (req, res) => {
+    const [orderId, amount, userId] = req.body;
+
+    const success = Math.random() > 0.2;
+
+    if (success) {
+        res.status(200)
+            .json({
+                orderId,
+                amount,
+                userId,
+                transactionId: Math.floor(Math.random() * 1000000),
+                status: 'PAID'
+            });
+    } else {
+        res.status(400).json({
+            orderId,
+            amount,
+            userId,
+            status: 'FAILED',
+            message: 'not authorized'
+        });
+    }
+});t
+
+module.exports = app;
